fbaffa323a45d11a5a2cd9aaf9861724063b0cd6
NatalieZeng <31214450+NatalieZeng@users.noreply.github.com>
2018-08-23 10:42:07 -0400

    Add files via upload

---
+++ FoodieToast/markers.js
@@ -0,0 +1,530 @@
+var places = [
+  {
+    "Boro": "Brooklyn",
+    "Name": "The Bagel Store",
+    "Address": "754 Metropolitan Ave., Brooklyn, NY 11211",
+    "Latitude, Longitude": "40.6501,  -73.9496"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Pyza",
+    "Address": "118 Nassau Ave., Brooklyn, NY 11222",
+    "Latitude, Longitude": "40.724319,  -73.948827"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "The Bagel Store",
+    "Address": "35 Orchard Street, New York, NY 10002",
+    "Latitude, Longitude": "40.715762,  -73.991557"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Pyza",
+    "Address": "447 7th Ave., Brooklyn, NY 11215",
+    "Latitude, Longitude": "40.662947,  -73.984683"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "The Bagel Store",
+    "Address": "Crown Heights, Bedford Stuyvesant, Brooklyn, NY 11216",
+    "Latitude, Longitude": "40.6834,  -73.9412"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Pyza",
+    "Address": "1318 Cortelyou Rd., Brooklyn, NY 11226",
+    "Latitude, Longitude": "40.640538, -73.965533"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "The Bagel Store",
+    "Address": "824B Washington Ave., Brooklyn, NY 11238",
+    "Latitude, Longitude": "40.673296, -73.963052"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Pyza",
+    "Address": "373 Lewis Ave., Brooklyn, NY 11233",
+    "Latitude, Longitude": "40.682663, -73.934813"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "The Bagel Store",
+    "Address": "235 Malcolm X Blvd., Brooklyn, NY 11233",
+    "Latitude, Longitude": "40.684727, -73.929279"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Pyza",
+    "Address": "203 Wilson Ave., Brooklyn, NY 11237",
+    "Latitude, Longitude": "40.699054, -73.922579"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "The Bagel Store",
+    "Address": "887 Fulton St., Brooklyn, NY 11238",
+    "Latitude, Longitude": "40.683768, -73.967678"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Sal Restaurant",
+    "Address": "810 Franklin Ave., Brooklyn, NY 11225",
+    "Latitude, Longitude": "40.670196, -73.958292"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Claypot Noodles Shop",
+    "Address": "5017 6th Ave., Brooklyn, NY 11220",
+    "Latitude, Longitude": "40.643257, -74.008447"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Roots Cafe",
+    "Address": "639 5th Ave., Brooklyn, NY 11215",
+    "Latitude, Longitude": "40.662844, -73.991662"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "dot & line",
+    "Address": "253 Bergen St., Brooklyn, NY 11217",
+    "Latitude, Longitude": "40.684143, -73.983592"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Harmony Tea House",
+    "Address": "161 Havemeyer St., Brooklyn, NY 11211",
+    "Latitude, Longitude": "40.711274, -73.957606"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Teadora",
+    "Address": "6609 Fort Hamilton Pkwy., Brooklyn, NY 11219",
+    "Latitude, Longitude": "40.629836, -74.010119"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Cloud 9 Crepes",
+    "Address": "434 Marcus Garvet Blvd., Brooklyn, NY 11216",
+    "Latitude, Longitude": "40.681876, -73.937966"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Together",
+    "Address": "2325 65th St., Brooklyn, NY 11204",
+    "Latitude, Longitude": "40.612203, -73.978403"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "DUB Pies",
+    "Address": "211 Prospect Park W, Brooklyn, NY 11215",
+    "Latitude, Longitude": "40.660425, -73.980166"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Daly Pie",
+    "Address": "665 Vanderbilt Ave., Brooklyn, NY 11201",
+    "Latitude, Longitude": "40.677022, -73.968886"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Lillo Cucina Italiana",
+    "Address": "331 Henry St., Brooklyn, NY 11249",
+    "Latitude, Longitude": "40.690228, -73.996342"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Taiyaki NYC",
+    "Address": "294 Bedford Ave., Brooklyn, NY 11249",
+    "Latitude, Longitude": "40.714574, -73.961212"
+  },
+  {
+    "Boro": "Brooklyn",
+    "Name": "Mia's Bakery",
+    "Address": "139 Smith St., Brooklyn, NY 11201",
+    "Latitude, Longitude": "40.687024, -73.990128"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Burger Lodge",
+    "Address": "657 E 189th St., Bronx, NY 10458",
+    "Latitude, Longitude": "40.856568, -73.884146"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Primavera Pizzeria",
+    "Address": "761 Morris Park Ave., Bronx, NY 10462",
+    "Latitude, Longitude": "40.845842, -73.863887"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Taqueria Tlaxicalli",
+    "Address": "2103 Starling Ave., Bronx, NY 10462",
+    "Latitude, Longitude": "40.836214, -73.854878"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Mega Seafood",
+    "Address": "1181 Morrison Ave., Bronx, NY 10472",
+    "Latitude, Longitude": "40.828931, -73.874614"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Pique 86",
+    "Address": "220 E 198th St., Bronx, NY 10458",
+    "Latitude, Longitude": "40.869591, -73.890223"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Nicky's Coffee Shop",
+    "Address": "3140 Bainbridge Ave., Bronx, NY 10467",
+    "Latitude, Longitude": "40.874867, -73.879275"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Cachapa Cachapa",
+    "Address": "1903 Bathgate Ave., Bronx, NY 10457",
+    "Latitude, Longitude": "40.847179,  -73.897138"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "George's Diner & Cafe",
+    "Address": "2369 Westchester Ave., Bronx, NY 10462",
+    "Latitude, Longitude": "40.836795, -73.846947"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Feroza's Restaurant Roti",
+    "Address": "716 Burke Ave., Bronx, NY 10467",
+    "Latitude, Longitude": "40.871277, -73.866448"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Com Tam Ninh Kieu",
+    "Address": "2641 Jerome Ave., Bronx, NY 10468",
+    "Latitude, Longitude": "40.866959, -73.898035"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Ez Grill NYC",
+    "Address": "790 Lydig Ave., Bronx, NY 10462",
+    "Latitude, Longitude": "40.854335, -73.864146"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Eating Tree",
+    "Address": "892 Gerard Ave., Bronx, NY 10452",
+    "Latitude, Longitude": "40.828121, -73.924575"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Jalisco Tacos",
+    "Address": "214 St. Anns Ave., Bronx, NY 10454",
+    "Latitude, Longitude": "40.806307, -73.917656"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "China Mia",
+    "Address": "2332 White Plains Rd., Bronx, NY 10467",
+    "Latitude, Longitude": "40.860273, -73.867198"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Landin Mac and Cheese",
+    "Address": "701 Melrose Ave., Bronx, NY 10455",
+    "Latitude, Longitude": "40.820008, -73.916151"
+  },
+  {
+    "Boro": "Bronx",
+    "Name": "Juice Rite",
+    "Address": "1274 Castle Hill Ave., Bronx, NY 10462",
+    "Latitude, Longitude": "40.834038, -73.851392"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "The Halal Guys",
+    "Address": "W 53rd St, New York, NY 10019",
+    "Latitude, Longitude": "40.764409, -73.985047"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Bibble & Sip",
+    "Address": "253 W 51st St, New York, NY 10019",
+    "Latitude, Longitude": "40.762824, -73.985178"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Los Tacos No. 1",
+    "Address": "229 W 43rd St, New York, NY 10036",
+    "Latitude, Longitude": "40.75719, -73.98751"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Best Bagel & Coffee",
+    "Address": "225 W 35th St A, New York, NY 10001",
+    "Latitude, Longitude": "40.752174, -73.991101"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Prince Street Pizza",
+    "Address": "27 Prince St A, New York, NY 10012",
+    "Latitude, Longitude": "40.723084,-73.994528"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Mamoun's Falafel",
+    "Address": "119 Macdougal St, New York, NY 10012",
+    "Latitude, Longitude": "40.730213, -74.000435"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Artichoke Basille's Pizza",
+    "Address": "111 Macdougal St, New York, NY 10012",
+    "Latitude, Longitude": "40.729949, -74.000656"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Ess-a-Bagel",
+    "Address": "831 3rd Ave, New York, NY 10022",
+    "Latitude, Longitude": "40.756193, 40.756193"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Taim",
+    "Address": "222 Waverly Pl, New York, NY 10014",
+    "Latitude, Longitude": "40.734858, -74.001497"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Woorjip",
+    "Address": "12 W 32nd St, New York, NY 10001",
+    "Latitude, Longitude": "40.747459, -73.986476"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Saigon Shack",
+    "Address": "114 Macdougal St, New York, NY 10012",
+    "Latitude, Longitude": "40.72969, -74.000632"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "McSorley's Ole Ale House",
+    "Address": "15 E 7th St, New York, NY 10003",
+    "Latitude, Longitude": "40.728757, -73.989709"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Schmackary's",
+    "Address": "362 W 45th St, New York, NY 10036",
+    "Latitude, Longitude": "40.760149, -73.990987"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "NY Pizza Suprema",
+    "Address": "413 8th Ave, New York, NY 10001",
+    "Latitude, Longitude": "40.750144, -73.995182"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Joe's Pizza",
+    "Address": "7 Carmine St, New York, NY 10014",
+    "Latitude, Longitude": "40.73055, -74.002126"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Breakroom Burgers & Tacos",
+    "Address": "83 Baxter St, New York, NY 10013",
+    "Latitude, Longitude": "40.716624, -73.999774"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Wah Fung Fast Food",
+    "Address": "79 Chrystie St, New York, NY 10002",
+    "Latitude, Longitude": "40.717307, -73.994454"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Cheeky Sandwiches",
+    "Address": "35 Orchard St, New York, NY 10002",
+    "Latitude, Longitude": "40.715762, -73.991557"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Vanessa's Dumpling House",
+    "Address": "220 E 14th St, New York, NY 10003",
+    "Latitude, Longitude": "40.732773,  -73.986362"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Xi'an Famous Foods",
+    "Address": "24 W 45th St, New York, NY 10036",
+    "Latitude, Longitude": "40.755849, -73.980782"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Num Pang Kitchen",
+    "Address": "5515, 140 E 41st St, New York, NY 10017",
+    "Latitude, Longitude": "40.752764, -73.981254"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Brooklyn Bagel & Coffee Company",
+    "Address": "286 8th Ave, New York, NY 10001",
+    "Latitude, Longitude": "40.74617, -73.997629"
+  },
+  {
+    "Boro": "Manhattan",
+    "Name": "Frisson Espresso",
+    "Address": "36 3rd Ave, New York, NY 10003",
+    "Latitude, Longitude": "40.730473, -73.989476"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Golden Corner Noodles",
+    "Address": "4215 College Point Blvd, Flushing, NY 11355",
+    "Latitude, Longitude": "40.7541649,-73.8327618"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Dumplings and Things",
+    "Address": "45-26 46th St, Sunnyside, NY 11104",
+    "Latitude, Longitude": "40.7398, -73.9354"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Garlic To The Chicken",
+    "Address": "6692 Forest Ave, Ridgewood, NY 11385",
+    "Latitude, Longitude": "40.7049523,-73.9022751"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Shah's Halal Food",
+    "Address": "3334 Hillside Avenue, New Hyde Park, NY 11040",
+    "Latitude, Longitude": "40.752644, -73.659902"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Burek's Pizza",
+    "Address": "6857 Forest Ave, Ridgewood, NY 11385",
+    "Latitude, Longitude": "40.7022336,-73.9007433"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Youth Cafe",
+    "Address": "201-01 Northern Blvd, Bayside, NY 11361",
+    "Latitude, Longitude": "40.757808,  -73.782168"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Sac's Place of Jackson Heights",
+    "Address": "86-14A 37th Ave, Jackson Heights, NY 11372",
+    "Latitude, Longitude": "40.7500901, -73.8799527"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "NuNoodle",
+    "Address": "89-10 Queens Blvd, Queens, NY 11373",
+    "Latitude, Longitude": "40.734403, -73.874492"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Tokyo Express",
+    "Address": "70-57 Parsons Blvd, Fresh Meadows, NY 11365",
+    "Latitude, Longitude": "40.730813, -73.8100618"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Caravan",
+    "Address": "35-01 Broadway, Queens, NY 11106",
+    "Latitude, Longitude": "40.760525, -73.922004"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Prince Noodle and Cafe",
+    "Address": "40-09 Prince St, Flushing, NY 11354",
+    "Latitude, Longitude": "40.758656, -73.831565"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Halal Munchies",
+    "Address": "69-21 164th St, Fresh Meadows, NY 11365",
+    "Latitude, Longitude": "40.7325881, -73.8045483"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Kimganae",
+    "Address": "3912 Union St, Flushing, NY 11354",
+    "Latitude, Longitude": "40.7607315,-73.8268925,"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Mr. Wonton",
+    "Address": "78-05 Jamaica Ave, Jamaica, NY 11421",
+    "Latitude, Longitude": "40.691954, -73.863957"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Burrito Republic",
+    "Address": "59-15 71st Ave, Ridgewood, NY 11385",
+    "Latitude, Longitude": "40.700763, -73.899227"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Entice Eats/Mac Shack Queens",
+    "Address": "168-18 Jamaica Ave, Jamaica, NY 11432",
+    "Latitude, Longitude": "40.706387, -73.791915"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Satyr Grill",
+    "Address": "1109 Cypress Ave, Ridgewood, NY 11385",
+    "Latitude, Longitude": "40.69604, -73.899474"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Don Tapas",
+    "Address": "Falchi Bldg., 31-00 47th Ave, Long Island City, NY 11101",
+    "Latitude, Longitude": "40.742853, -73.935366"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Potala Fresh Momo",
+    "Address": "Broadway and 37th Ave, Jackson Heights, NY 11372",
+    "Latitude, Longitude": "40.750116, -73.881958"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Khao Kang",
+    "Address": "76-20 Woodside Ave, Elmhurst, NY 11373",
+    "Latitude, Longitude": "40.743406, -73.888086"
+  },
+  {
+    "Boro": "Queens",
+    "Name": "Five Loaves and Two Fishes Sushi",
+    "Address": "82-72 Broadway, Queens, NY 11373",
+    "Latitude, Longitude": "40.7498, -73.7976"
+  }
+]
+
+createMarkers = function(map){
+  for(var i = 0;i<places.length;i++){
+      createMarker(i);
+  }
+  function createMarker(i){
+    var lonlat = places[i]['Latitude, Longitude'].split(',')
+    console.log(lonlat)
+    var latlng = {lat:parseFloat(lonlat[0]),lng:parseFloat(lonlat[1])};
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map:map,
+      title: places[i]['Name']
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: places[i]['Address'],
+      maxWidth: 400,
+      maxHeight: 5
+    });
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  }
+}
